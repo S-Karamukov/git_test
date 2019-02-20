5cbb484b84625123ad38bc9ad1f42e98f3bc55c6
S-Karamukov <svetliork_99@abv.bg>
2019-02-20 13:27:25 +0200

    Initial Commit

---
+++ doodle1/Kursova_Rabota/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kursova_Rabota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tuka mi e kursovata rabota");
+        }
+    }
+}
---
+++ doodle1/Programa_A+B/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Programa_A_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 6;
+            Console.WriteLine(a+b);
+        }
+    }
+}
